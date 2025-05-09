1ed278c3889d0599b0b5ac38846091895c24399c
wjbetech <wjbetech@github.com>
2025-05-09 17:25:37 +0900

    Pushing working code up to Part 11, ex. 11.9

---
+++ fso-pokedex/tests/firstTest.spec.js
@@ -0,0 +1,18 @@
+import { test, describe, expect, beforeEach } from "@playwright/test";
+
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+
+  test("can navigate to a pokemon", async ({ page }) => {
+    await page.goto("/pokemon/ivysaur");
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+  });
+});
