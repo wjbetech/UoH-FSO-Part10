0673a5a0ea9e5ad998804ce3b1c94d427640bcab
wjbetech <wjbetech@gmail.com>
2025-05-09 12:11:06 +0900

    Fixing pipeline action issues v2

--- fso-pokedex/app.js
+++ fso-pokedex/app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
 });
