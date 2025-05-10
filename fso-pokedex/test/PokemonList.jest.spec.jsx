ee95bace8966399c145b4b23c12a4277e006e63a
wjbetech <wjbetech@gmail.com>
2025-05-10 20:51:29 +0900

    Fixing playwright test package issues v6

--- fso-pokedex/test/PokemonList.jest.spec.jsx
+++ fso-pokedex/test/PokemonList.jest.spec.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
 import PokemonList from "../src/PokemonList";
 
 const pokemonList = [
--- fso-pokedex/test/PokemonPage.jest.spec.jsx
+++ fso-pokedex/test/PokemonPage.jest.spec.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import axiosMock from "axios";
 import { act } from "react-dom/test-utils";
-import "@testing-library/jest-dom";
 import PokemonPage from "../src/PokemonPage";
 
 import { MemoryRouter } from "react-router-dom";
