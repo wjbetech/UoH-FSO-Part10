7b4f09197095825482a1d24b1a5743d95b87515e
wjbetech <wjbetech@gmail.com>
2025-05-10 22:07:02 +0900

    fixing git

--- fso-pokedex/.eslintrc.js
+++ fso-pokedex/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",
