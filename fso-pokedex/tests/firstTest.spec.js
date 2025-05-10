e997d26a72dc30ccf555de4c90c9d9fb4d4c5b40
wjbetech <wjbetech@gmail.com>
2025-05-10 23:31:38 +0900

    Trying to fix the test errors again

--- fso-pokedex/tests/firstTest.spec.js
+++ fso-pokedex/tests/firstTest.spec.js
@@ -1,15 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Pokedex", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("/");
-    await expect(page.getByText("ivysaur")).toBeVisible();
-    await expect(page.getByText("Pokémon and Pokémon character names are trademarks of Nintendo.")).toBeVisible();
-  });
+test("front page can be opened", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByText("ivysaur")).toBeVisible();
+  await expect(page.getByText("Pokémon and Pokémon character names are trademarks of Nintendo.")).toBeVisible();
+});
 
-  test("can navigate to a pokemon", async ({ page }) => {
-    await page.goto("/pokemon/ivysaur");
-    const abilityText = await page.getByText("chlorophyll");
-    await expect(abilityText).toBeVisible();
-  });
+test("can navigate to a pokemon", async ({ page }) => {
+  await page.goto("/pokemon/ivysaur");
+  const abilityText = await page.getByText("chlorophyll");
+  await expect(abilityText).toBeVisible();
 });
